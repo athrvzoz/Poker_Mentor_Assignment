ebb1c395f49ca6a1f0416e2951ff255b87dad0f8
Atharva Vaibhav Deshmukh <atharva.deshmukh@zensar.com>
2025-04-18 16:12:37 +0530

    add calculations

---
+++ testdata.js
@@ -0,0 +1,31 @@
+royalflush=[
+    { id: 9, value: '10', suit: 'Clubs', src: '/images/clubs_10.png' },
+    { id: 10, value: '11', suit: 'Clubs', src: '/images/clubs_J.png' },
+    { id: 11, value: '12', suit: 'Clubs', src: '/images/clubs_Q.png' },
+    { id: 12, value: '13', suit: 'Clubs', src: '/images/clubs_K.png' },
+    { id: 13, value: '14', suit: 'Clubs', src: '/images/clubs_A.png' },
+]
+
+four_Aces=[
+    { id: 13, value: '14', suit: 'Clubs', src: '/images/clubs_A.png' },
+    { id: 26, value: '14', suit: 'Diamonds', src: '/images/diamonds_A.png' },
+    { id: 39, value: '14', suit: 'Hearts', src: '/images/hearts_A.png' },
+    { id: 52, value: '14', suit: 'Spades', src: '/images/spades_A.png' },
+    { id: 40, value: '2', suit: 'Spades', src: '/images/spades_2.png' },
+    { id: 37, value: '12', suit: 'Hearts', src: '/images/hearts_Q.png' },
+]
+
+fullhouse=[
+    
+    { id: 26, value: '14', suit: 'Diamonds', src: '/images/diamonds_A.png' },
+
+]
+
+twopair=[
+    { id: 13, value: '14', suit: 'Clubs', src: '/images/clubs_A.png' },
+    { id: 26, value: '14', suit: 'Diamonds', src: '/images/diamonds_A.png' },
+    { id: 11, value: '12', suit: 'Clubs', src: '/images/clubs_Q.png' },
+    { id: 37, value: '12', suit: 'Hearts', src: '/images/hearts_Q.png' },
+    { id: 9, value: '10', suit: 'Clubs', src: '/images/clubs_10.png' },
+    { id: 10, value: '11', suit: 'Clubs', src: '/images/clubs_J.png' }
+]
