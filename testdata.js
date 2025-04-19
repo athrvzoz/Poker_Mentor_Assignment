305cf61564e18b1f31a947a81b75e329b05ff72e
Atharva Vaibhav Deshmukh <atharva.deshmukh@zensar.com>
2025-04-19 18:26:11 +0530

    Add ui and MODIFY buttons

--- testdata.js
+++ testdata.js
@@ -16,9 +16,12 @@ four_Aces=[
 ]
 
 fullhouse=[
-    
+    { id: 36, value: '11', suit: 'Hearts', src: '/images/hearts_J.png' },
+    { id: 51, value: '13', suit: 'Spades', src: '/images/spades_K.png' },
     { id: 26, value: '14', suit: 'Diamonds', src: '/images/diamonds_A.png' },
-
+    { id: 12, value: '13', suit: 'Clubs', src: '/images/clubs_K.png' },
+    { id: 5, value: '6', suit: 'Clubs', src: '/images/clubs_6.png' },
+    { id: 6, value: '7', suit: 'Clubs', src: '/images/clubs_7.png' }
 ]
 
 twopair=[
@@ -29,3 +32,84 @@ twopair=[
     { id: 9, value: '10', suit: 'Clubs', src: '/images/clubs_10.png' },
     { id: 10, value: '11', suit: 'Clubs', src: '/images/clubs_J.png' }
 ]
+
+four_of__kind=[
+    { id: 5, value: '6', suit: 'Clubs', src: '/images/clubs_6.png' },
+    { id: 18, value: '6', suit: 'Diamonds', src: '/images/diamonds_6.png' },
+    { id: 31, value: '6', suit: 'Hearts', src: '/images/hearts_6.png' },
+    { id: 44, value: '6', suit: 'Spades', src: '/images/spades_6.png' },
+    { id: 40, value: '2', suit: 'Spades', src: '/images/spades_2.png' },
+    { id: 41, value: '3', suit: 'Spades', src: '/images/spades_3.png' },
+    
+]
+
+
+threeofakind=[
+    { id: 14, value: '2', suit: 'Diamonds', src: '/images/diamonds_2.png' },
+    { id: 1, value: '2', suit: 'Clubs', src: '/images/clubs_2.png' },
+    { id: 27, value: '2', suit: 'Hearts', src: '/images/hearts_2.png' },
+    { id: 8, value: '9', suit: 'Clubs', src: '/images/clubs_9.png' },
+    { id: 9, value: '10', suit: 'Clubs', src: '/images/clubs_10.png' },
+    { id: 50, value: '12', suit: 'Spades', src: '/images/spades_Q.png' }
+]
+const allCardImages = [
+    // Clubs
+    { id: 1, value: '2', suit: 'Clubs', src: '/images/clubs_2.png' },
+    { id: 2, value: '3', suit: 'Clubs', src: '/images/clubs_3.png' },
+    { id: 3, value: '4', suit: 'Clubs', src: '/images/clubs_4.png' },
+    { id: 4, value: '5', suit: 'Clubs', src: '/images/clubs_5.png' },
+    { id: 5, value: '6', suit: 'Clubs', src: '/images/clubs_6.png' },
+    { id: 6, value: '7', suit: 'Clubs', src: '/images/clubs_7.png' },
+    { id: 7, value: '8', suit: 'Clubs', src: '/images/clubs_8.png' },
+    { id: 8, value: '9', suit: 'Clubs', src: '/images/clubs_9.png' },
+    { id: 9, value: '10', suit: 'Clubs', src: '/images/clubs_10.png' },
+    { id: 10, value: '11', suit: 'Clubs', src: '/images/clubs_J.png' },
+    { id: 11, value: '12', suit: 'Clubs', src: '/images/clubs_Q.png' },
+    { id: 12, value: '13', suit: 'Clubs', src: '/images/clubs_K.png' },
+    { id: 13, value: '14', suit: 'Clubs', src: '/images/clubs_A.png' },
+
+    // Diamonds
+    { id: 14, value: '2', suit: 'Diamonds', src: '/images/diamonds_2.png' },
+    { id: 15, value: '3', suit: 'Diamonds', src: '/images/diamonds_3.png' },
+    { id: 16, value: '4', suit: 'Diamonds', src: '/images/diamonds_4.png' },
+    { id: 17, value: '5', suit: 'Diamonds', src: '/images/diamonds_5.png' },
+    { id: 18, value: '6', suit: 'Diamonds', src: '/images/diamonds_6.png' },
+    { id: 19, value: '7', suit: 'Diamonds', src: '/images/diamonds_7.png' },
+    { id: 20, value: '8', suit: 'Diamonds', src: '/images/diamonds_8.png' },
+    { id: 21, value: '9', suit: 'Diamonds', src: '/images/diamonds_9.png' },
+    { id: 22, value: '10', suit: 'Diamonds', src: '/images/diamonds_10.png' },
+    { id: 23, value: '11', suit: 'Diamonds', src: '/images/diamonds_J.png' },
+    { id: 24, value: '12', suit: 'Diamonds', src: '/images/diamonds_Q.png' },
+    { id: 25, value: '13', suit: 'Diamonds', src: '/images/diamonds_K.png' },
+    { id: 26, value: '14', suit: 'Diamonds', src: '/images/diamonds_A.png' },
+
+    // Hearts
+    { id: 27, value: '2', suit: 'Hearts', src: '/images/hearts_2.png' },
+    { id: 28, value: '3', suit: 'Hearts', src: '/images/hearts_3.png' },
+    { id: 29, value: '4', suit: 'Hearts', src: '/images/hearts_4.png' },
+    { id: 30, value: '5', suit: 'Hearts', src: '/images/hearts_5.png' },
+    { id: 31, value: '6', suit: 'Hearts', src: '/images/hearts_6.png' },
+    { id: 32, value: '7', suit: 'Hearts', src: '/images/hearts_7.png' },
+    { id: 33, value: '8', suit: 'Hearts', src: '/images/hearts_8.png' },
+    { id: 34, value: '9', suit: 'Hearts', src: '/images/hearts_9.png' },
+    { id: 35, value: '10', suit: 'Hearts', src: '/images/hearts_10.png' },
+    { id: 36, value: '11', suit: 'Hearts', src: '/images/hearts_J.png' },
+    { id: 37, value: '12', suit: 'Hearts', src: '/images/hearts_Q.png' },
+    { id: 38, value: '13', suit: 'Hearts', src: '/images/hearts_K.png' },
+    { id: 39, value: '14', suit: 'Hearts', src: '/images/hearts_A.png' },
+
+    // Spades
+    { id: 40, value: '2', suit: 'Spades', src: '/images/spades_2.png' },
+    { id: 41, value: '3', suit: 'Spades', src: '/images/spades_3.png' },
+    { id: 42, value: '4', suit: 'Spades', src: '/images/spades_4.png' },
+    { id: 43, value: '5', suit: 'Spades', src: '/images/spades_5.png' },
+    { id: 44, value: '6', suit: 'Spades', src: '/images/spades_6.png' },
+    { id: 45, value: '7', suit: 'Spades', src: '/images/spades_7.png' },
+    { id: 46, value: '8', suit: 'Spades', src: '/images/spades_8.png' },
+    { id: 47, value: '9', suit: 'Spades', src: '/images/spades_9.png' },
+    { id: 48, value: '10', suit: 'Spades', src: '/images/spades_10.png' },
+    { id: 49, value: '11', suit: 'Spades', src: '/images/spades_J.png' },
+    { id: 50, value: '12', suit: 'Spades', src: '/images/spades_Q.png' },
+    { id: 51, value: '13', suit: 'Spades', src: '/images/spades_K.png' },
+    { id: 52, value: '14', suit: 'Spades', src: '/images/spades_A.png' }
+];
